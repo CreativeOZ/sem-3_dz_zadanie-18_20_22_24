2543a2c16415a3a6721d3850c4888fbf7ecbe62f
CreativeOZ <zagriaoleg@gmail.com>
2022-02-24 17:49:58 +0300

    Проверить истинность утверждения.

---
+++ zadanie_18/Program.cs
@@ -0,0 +1,11 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y.
+
+Console.Clear();
+
+bool x = true;
+bool y = true;
+
+if (!(x || y) == (!x && !y))
+Console.WriteLine("Является истиной");
+else
+Console.WriteLine("Не является истиной");
