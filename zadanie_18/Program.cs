d79e1ef45d336684dcb7ff256709e46d6996e450
CreativeOZ <zagriaoleg@gmail.com>
2022-02-25 13:56:31 +0300

    Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y.

--- zadanie_18/Program.cs
+++ zadanie_18/Program.cs
@@ -2,10 +2,41 @@
 
 Console.Clear();
 
+bool z1 = true;
+bool z2 = true;
+bool z3 = true;
+bool z4 = true;
+
 bool x = true;
 bool y = true;
 
 if (!(x || y) == (!x && !y))
-Console.WriteLine("Является истиной");
+{
+    z1 = true;
+}
+x = false;
+y = false;
+
+if (!(x || y) == (!x && !y))
+{
+    z2 = true;
+}
+x = true;
+y = false;
+
+if (!(x || y) == (!x && !y))
+{
+    z3 = true;
+}
+x = false;
+y = true;
+
+if (!(x || y) == (!x && !y))
+{
+    z4 = true;
+}
+if (z1 && z2 && z3 && z4) Console.WriteLine("Является истиной");
 else
+{
 Console.WriteLine("Не является истиной");
+}
